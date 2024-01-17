2337071c0ac6195195cdd73077cdc52e6b6a1101
Ezoyles <llamaegg2@gmail.com>
2024-01-18 00:10:17 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,85 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.random.RandomGenerator;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(calc(input));
+    }
+    static String[] actions = {String.valueOf('+'), "-", "/", String.valueOf('*')};
+    static String[] regexActions = {"\\+", "-", "/", "\\*"};
+    public static String calc(String input) throws Exception {
+        int index = -1;
+        for (int i = 0; i < actions.length; i++) {
+            if (input.contains(actions[i])) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            throw new Exception("Неизвестный оператор");
+        }
+        String[] data = input.split(regexActions[index]);
+        Converter converter = new Converter();
+        if(converter.isRoman(data[0]) == converter.isRoman(data[1])) {
+            int a, b;
+            //Определяем, римские ли это числа
+            boolean isRoman = converter.isRoman(data[0]);
+            if (isRoman) {
+                //если римские, то конвертируем их в арабские
+                //X+V
+                //x-10
+                //v - 5
+                a = converter.romanToInt(data[0]);
+                if (a > 10 || a < 1) {
+                    throw new Exception("На входе обнаружено число >10");
+                }
+                b = converter.romanToInt(data[1]);
+                if (b > 10 || b < 1) {
+                    throw new Exception("На входе обнаружено число >10");
+                }
+
+            } else {
+                //если арабские, конвертируем их из строки в число
+                a = Integer.parseInt(data[0]);
+                if (a > 10 || a < 1) {
+                    throw new Exception("На входе обнаружено число >10");
+                }
+                b = Integer.parseInt(data[1]);
+                if (b > 10 || b < 1) {
+                    throw new Exception("На входе обнаружено число >10");
+                }
+            }
+            //выполняем с числами арифметическое действие
+            int result;
+            switch (actions[index]) {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                default:
+                    result = a / b;
+                    break;
+            }
+            //15->XV
+            if (isRoman) {
+                //если числа были римские, возвращаем результат в римском числе
+                return (converter.intToRoman(result));
+            } else {
+                //если числа были арабские, возвращаем результат в арабском числе
+                return String.valueOf(result);
+            }
+        }
+        throw new Exception ("Числа должны быть в одном формате");
+    }
+}
